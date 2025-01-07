b336f4612dfdc68794da97bd87084ef106c32275
Conan Bitter <conan.bitter@gmail.com>
2025-01-07 14:02:46 +0300

    Add screen scaling

--- conf.lua
+++ conf.lua
@@ -1,8 +1,8 @@
 function love.conf(t)
     t.window.title = "Klondike"
-    --t.window.width = 800
-    --t.window.height = 600
-    t.window.resizable = true
+    t.window.width = 350 * 3
+    t.window.height = 300 * 3
+    t.window.resizable = false
     t.window.minwidth = 350
     t.window.minheight = 300
     t.window.vsync = 1
--- main.lua
+++ main.lua
@@ -2,12 +2,18 @@ local cards = require "cards"
 
 function love.load()
     love.graphics.setBackgroundColor(62 / 255, 140 / 255, 54 / 255)
+    love.graphics.setDefaultFilter("nearest", "nearest")
     cards.load()
 end
 
 function love.draw()
+    -- scaling
+    love.graphics.push()
+    love.graphics.scale(3, 3)
+
     cards.draw(cards.back, 10, 10)
     cards.draw(cards.placeholder_homes[cards.Suit.Clubs], 100, 10)
-
     cards.draw_card(cards.Suit.Diamonds, cards.Rank.Queen, 10, 200)
+
+    love.graphics.pop()
 end
