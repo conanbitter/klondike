786ecef8a293220f966c254b170dd1d481adf6aa
Conan Bitter <conan.bitter@gmail.com>
2025-01-07 17:31:42 +0300

    Begin working on decks

--- decks.lua
+++ decks.lua
@@ -16,8 +16,63 @@ local function new_card(suit, rank)
     }
 end
 
+---@class Deck
+---@field x number
+---@field y number
+---@field cards Card[]
+---@field placeholder love.Quad
+---@field draw function()
+
+---@param self Deck
+local function draw_deck(self)
+    cards.draw(self.placeholder, self.x, self.y)
+end
+
+---@param x number
+---@param y number
+---@return Deck
+local function new_deck(x, y)
+    return {
+        x = x,
+        y = y,
+        cards = {},
+        placeholder = cards.placeholder_empty,
+        draw = draw_deck,
+    }
+end
+
+local FLAT_OFFSET = 14
+
+---@class FlatDeck:Deck
+---@field covered boolean
+
+---@param self FlatDeck
+local function draw_flat(self)
+    if #self.cards == 0 then
+        cards.draw(self.placeholder, self.x, self.y)
+    end
+    for i, card in ipairs(self.cards) do
+        if self.covered and i < #self.cards then
+            cards.draw(cards.back, self.x, self.y + (i - 1) * FLAT_OFFSET)
+        else
+            cards.draw_card(card, self.x, self.y + (i - 1) * FLAT_OFFSET)
+        end
+    end
+end
+
+---@param x number
+---@param y number
+---@return FlatDeck
+local function new_flat_deck(x, y)
+    local result = new_deck(x, y) --[[@as FlatDeck]]
+    result.covered = true
+    result.draw = draw_flat
+    return result
+end
+
 function module.init()
     local main_deck = {}
+    local decks = {}
 
     for suit = 1, 4 do
         for rank = 1, 13 do
@@ -25,7 +80,15 @@ function module.init()
         end
     end
 
-    return main_deck[1]
+    local test_deck = new_flat_deck(10, 10)
+    for i = 1, 10 do
+        table.insert(test_deck.cards, main_deck[#main_deck])
+        table.remove(main_deck, #main_deck)
+    end
+
+    table.insert(decks, test_deck)
+
+    return decks
 end
 
 return module
--- main.lua
+++ main.lua
@@ -5,13 +5,14 @@ end
 local cards = require "cards"
 local decks = require "decks"
 
-local temp
+---@type Deck[]
+local deck_list
 
 function love.load()
     love.graphics.setBackgroundColor(62 / 255, 140 / 255, 54 / 255)
     love.graphics.setDefaultFilter("nearest", "nearest")
     cards.init()
-    temp = decks.init()
+    deck_list = decks.init()
 end
 
 function love.draw()
@@ -21,9 +22,12 @@ function love.draw()
 
     cards.draw(cards.back, 10, 10)
     cards.draw(cards.placeholder_homes[Suit.Clubs], 100, 10)
+    for _, deck in ipairs(deck_list) do
+        deck:draw()
+    end
 
-    local mx, my = love.graphics.inverseTransformPoint(love.mouse.getPosition())
-    cards.draw_card(temp, mx - CARD_WIDTH / 2, my - CARD_HEIGHT / 2)
+    --local mx, my = love.graphics.inverseTransformPoint(love.mouse.getPosition())
+    --cards.draw_card(temp, mx - CARD_WIDTH / 2, my - CARD_HEIGHT / 2)
 
     love.graphics.pop()
 end
