5f4fbdd7767f81bc100593a700627ac6df1bcc81
Conan Bitter <conan.bitter@gmail.com>
2025-01-07 12:58:10 +0300

    Add sprites

--- main.lua
+++ main.lua
@@ -1,3 +1,11 @@
+---@type love.Image
+local sprites
+
+function love.load()
+    sprites = love.graphics.newImage("sprites.png")
+end
+
 function love.draw()
     love.graphics.print("Hello World", 400, 300)
+    love.graphics.draw(sprites, 20, 20)
 end
