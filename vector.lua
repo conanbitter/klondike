5dd6bf0f176508008f0e58a7ec25aa47503a9097
Conan Bitter <conan.bitter@gmail.com>
2025-01-11 11:30:09 +0300

    Add vector mini library

---
+++ vector.lua
@@ -0,0 +1,27 @@
+local module = {}
+
+---@class Vector
+---@field x number
+---@field y number
+---@field distance2 fun():number
+
+---comment
+---@param self Vector
+---@param other Vector
+---@return number
+local function vector_distance2(self, other)
+    return (self.x - other.x) * (self.x - other.x) + (self.y - other.y) * (self.y - other.y)
+end
+
+---@param x number
+---@param y number
+---@return Vector
+function module.new_vector(x, y)
+    return {
+        x = x,
+        y = y,
+        distance2 = vector_distance2
+    }
+end
+
+return module
