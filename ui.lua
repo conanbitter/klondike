d9238adc3db653624449d5078f5a6e40d0186fa4
Conan Bitter <conan.bitter@gmail.com>
2025-01-12 15:51:22 +0300

    Begin working on UI

---
+++ ui.lua
@@ -0,0 +1,52 @@
+local Object = require "lib.classic"
+
+---@type love.Image
+local ui_texture
+
+---@class UIElement
+---@field x number
+---@field y number
+---@field width number
+---@field height number
+---@field state number
+---@field faces love.Quad[]
+---@field is_inside fun(self:UIElement, x:number, y:number):boolean
+---@field draw fun(self:UIElement)
+---@field on_mouse_move fun(self:UIElement, x:number, y:number)
+---@field on_mouse_down fun(self:UIElement, x:number, y:number):boolean
+---@field on_mouse_up fun(self:UIElement, x:number, y:number)
+---@overload fun(x:number, y:number, width:number, height:number, faces:Vector[])
+local UIElement = Object:extend()
+
+---@param x number
+---@param y number
+---@param width number
+---@param height number
+---@param faces Vector[]
+function UIElement:new(x, y, width, height, faces)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.faces = {}
+    for _, face in ipairs(faces) do
+        table.insert(self.faces, love.graphics.newQuad(face.x, face.y, width, height, ui_texture))
+    end
+    self.state = 1
+end
+
+function UIElement:is_inside(x, y)
+    return x >= self.x and y >= self.y and x < self.x + self.width and y < self.y + self.height
+end
+
+function UIElement:draw()
+    love.graphics.draw(ui_texture, self.faces[self.state], self.x, self.y)
+end
+
+function UIElement:on_mouse_move(x, y) end
+
+function UIElement:on_mouse_down(x, y)
+    return self:is_inside(x, y)
+end
+
+function UIElement:on_mouse_up(x, y) end
