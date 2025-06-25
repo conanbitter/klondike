b3a30666cb8c9e9dcb6aa857d3811b3d6b3ce49b
Conan Bitter <conan.bitter@gmail.com>
2025-06-25 21:20:26 +0300

    Add pixels class

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { Vec2 } from "./geometry";
 import * as atlas from "./atlas";
+import { Pixels } from "./pixels";
+
+let pixels: Pixels;
 
 love.load = () => {
+    love.graphics.setBackgroundColor(62 / 255, 140 / 255, 54 / 255);
+    love.graphics.setDefaultFilter("nearest", "nearest");
+    pixels = new Pixels();
+    pixels.setScale(3);
     atlas.InitAtlas();
 }
 
 love.draw = () => {
+    pixels.begin();
+    love.graphics.clear();
     atlas.Draw(atlas.CARDS[0][2], 100, 100)
+    pixels.finish();
 };
---
+++ src/pixels.ts
@@ -0,0 +1,56 @@
+import { Canvas, DisplayFlags } from "love.graphics";
+import { SCREEN_HEIGHT, SCREEN_WIDTH } from "./conf";
+import { Vec2 } from "./geometry";
+
+const DEFAULT_FLAGS: DisplayFlags = {
+    minwidth: SCREEN_WIDTH,
+    minheight: SCREEN_HEIGHT,
+    resizable: true,
+    vsync: true,
+}
+
+export class Pixels {
+    private canvas: Canvas;
+    private scale: number = 1;
+    private posX: number = 0;
+    private posY: number = 0;
+    private oldWidth: number = 0;
+    private oldHeight: number = 0;
+
+    constructor() {
+        this.canvas = love.graphics.newCanvas(SCREEN_WIDTH, SCREEN_HEIGHT);
+    }
+
+    begin() {
+        love.graphics.setCanvas(this.canvas);
+    }
+
+    finish() {
+        love.graphics.setCanvas()
+        love.graphics.draw(this.canvas, this.posX, this.posY, undefined, this.scale, this.scale);
+    }
+
+    setScale(newScale: number) {
+        this.scale = newScale;
+        this.posX = 0;
+        this.posY = 0;
+        this.oldWidth = SCREEN_WIDTH * this.scale;
+        this.oldHeight = SCREEN_HEIGHT * this.scale;
+        love.window.setMode(this.oldWidth, this.oldHeight, DEFAULT_FLAGS);
+    }
+
+    updateSize(width: number, height: number) {
+        if (width == this.oldWidth && height == this.oldHeight) return;
+        this.scale = math.floor(math.min(width / SCREEN_WIDTH, height / SCREEN_HEIGHT));
+        this.posX = math.floor((width - SCREEN_WIDTH * this.scale) / 2);
+        this.posY = math.floor((height - SCREEN_HEIGHT * this.scale) / 2);
+        this.oldWidth = width;
+        this.oldHeight = height;
+    }
+
+    posTo(pos: Vec2) {
+        return new Vec2(
+            math.floor((pos.x - this.posX) / this.scale),
+            math.floor((pos.y - this.posY) / this.scale));
+    }
+}
