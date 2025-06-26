a388c1c116937dcc69e5b38d9dd8658df8c94d92
Conan Bitter <conan.bitter@gmail.com>
2025-06-26 20:48:30 +0300

    Implement Rect

--- src/geometry.ts
+++ src/geometry.ts
@@ -32,3 +32,29 @@ export class Vec2 {
         return new Vec2((to.x - this.x) / distance, (to.y - this.y) / distance);
     }
 }
+
+class Rect {
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+
+    constructor(x: number, y: number, w: number, h: number) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    isInbounds(point: Vec2): boolean {
+        return point.x >= this.x &&
+            point.x <= this.x + this.h &&
+            point.y >= this.y &&
+            point.y <= this.y + this.h;
+    }
+
+    isIntersect(other: Rect): boolean {
+        return (this.x <= other.x + other.w && this.x + this.w >= other.x) &&
+            (this.y <= other.y + other.h && this.y + this.h >= other.y);
+    }
+}
