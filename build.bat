f4d92b9f0bc29388fe825791d4a3a6f79229f430
Conan Bitter <conan.bitter@gmail.com>
2025-08-24 11:38:48 +0300

    Add exe trim

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-dotnet publish -c Release -r win-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false -p:PublishAot=true --self-contained
+dotnet publish -c Release -r win-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false -p:PublishAot=true -p:PublishTrimmed=true --self-contained
